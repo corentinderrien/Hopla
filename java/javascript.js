accceaa0b0ac0ed6f7620fdd9e9306ce92c6ac56
Corentin Derrien <58248785+corentinderrien@users.noreply.github.com>
2021-02-11 12:06:28 +0100

    Add files via upload

---
+++ java/javascript.js
@@ -0,0 +1,43 @@
+var divs = document.querySelectorAll('.nav-link');
+window.onscroll = function () {
+
+    if (document.documentElement.scrollTop > 80) {
+        document.querySelector('.navbar').classList.add('navbar2');
+        for (var i = 0; i < divs.length; i++) {
+            divs[i].classList.add('nav-link2');
+        }
+    }
+    else {
+        document.querySelector('.navbar').classList.remove('navbar2');
+        for (var i = 0; i < divs.length; i++) {
+            divs[i].classList.remove('nav-link2');
+        }
+    }
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.decouverte').classList.add('decouverte2');
+        document.querySelector('.txtaccueil').classList.add('txtaccueil2');
+    }
+    else {
+        document.querySelector('.decouverte').classList.remove('decouverte2');
+        document.querySelector('.txtaccueil').classList.remove('txtaccueil2');
+    }
+
+    if (document.documentElement.scrollTop > 10) {
+        document.querySelector('.fristcontent').classList.add('fristcontent2');
+    }
+    else {
+        document.querySelector('.fristcontent').classList.remove('fristcontent2');
+
+    }
+    if (document.documentElement.scrollTop > 1400) {
+        document.querySelector('.tagline_contact').classList.add('tagline_contact2');
+    }
+    else {
+        document.querySelector('.tagline_contact').classList.remove('tagline_contact2');
+
+    }
+
+}
+
+
+
